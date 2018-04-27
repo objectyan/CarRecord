38db656abcd4025669e7cf72ab6b4dc992c5a448
OY <objecttozero@sina.com>
2018-04-27 16:10:38 +0800

    update chart name to analysis

---
+++ CarRecord/Controller/AnalysisController.swift
@@ -0,0 +1,27 @@
+//
+//  ChartController.swift
+//  CarRecord
+//
+//  Created by Object Yan on 2018/4/26.
+//  Copyright © 2018年 Object Yan. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class ChartController: UIViewController, ChartViewDelegate {
+    var chartView: LineChartView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.initializeForm()
+    }
+    
+    private func initializeForm(){
+        chartView = LineChartView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height*0.75));
+        self.view.addSubview(chartView)
+        
+    }
+    
+}
